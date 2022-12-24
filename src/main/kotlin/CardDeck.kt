0108d323502fe2093124ac2f917f59cb04ee3cf3
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-24 11:30:13 +0300

    create a method to get the default card value.

--- src/main/kotlin/CardDeck.kt
+++ src/main/kotlin/CardDeck.kt
@@ -13,3 +13,22 @@ class CardDeck {
 
 }
 
+fun getDefaultCardValues(cardRank:String): Int{
+    val value = when(cardRank){
+        "2" -> 2
+        "3" -> 3
+        "4" -> 4
+        "5" -> 5
+        "6" -> 6
+        "7" -> 7
+        "8" -> 8
+        "9" -> 9
+        "10" -> 10
+        "J" -> 11
+        "Q" -> 12
+        "K" -> 13
+        "A" -> 1
+        else -> {0}
+    }
+    return value
+}
