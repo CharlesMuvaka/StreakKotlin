56c8d5f7790306933d0630410fb7a2cceab28b51
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-24 07:36:09 +0300

    create an extensive function to check if cards are matching

--- src/main/kotlin/Card.kt
+++ src/main/kotlin/Card.kt
@@ -1,6 +1,18 @@
 //create a blueprint to initialise a card instance
 class Card(rank:String, value:Int, suit: CardSuit) {
-    val _rank: String = rank
-    val _value: Int = value
-    val _suit: CardSuit = suit
+    val cardRank: String = rank
+    val cardValue: Int = value
+    val cardSuit: CardSuit = suit
+}
+
+fun Card.checkCardMatching(card: Card): Boolean{
+    val checkRanks = this.cardRank == card.cardRank
+    val checkValues  = this.cardValue == card.cardValue
+    val checkSuitColor = this.cardSuit._color == card.cardSuit._color
+    val checkSuitName = this.cardSuit._name == card.cardSuit._name
+
+    if (checkRanks && checkSuitName && checkValues && checkSuitColor){
+        return true
+    }
+    return false
 }
