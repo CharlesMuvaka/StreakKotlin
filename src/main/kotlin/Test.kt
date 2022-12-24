88a8c350401bef89b4ea9624dddce36e5ad371f4
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-24 10:13:41 +0300

    create a test to check if the dynamic array shuffles cards.

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,9 +1,9 @@
 fun main() {
     val card = Card("2", 2,CardSuit("hearts", "red"))
-    val cardOne = Card("2", 2,CardSuit("hearts", "red"))
-    val cardTwo = Card("2", 2,CardSuit("hearts", "red"))
-//    println(card.cardRank)
-//    println(card.checkCardMatching(cardOne))
+    val cardOne = Card("3", 3,CardSuit("spades", "black"))
+    val cardTwo = Card("4", 3,CardSuit("clubs", "red"))
+    println(card.cardRank)
+    println(card.checkCardMatching(cardOne))
 
     val array:DynamicArray = DynamicArray()
 
@@ -13,5 +13,10 @@ fun main() {
     println(array.cards.size)
     array.removeCard(card)
     println(array.cards.size)
+    println(array.cards[0].cardRank)
+
+    array.shuffleCards()
+
+    println(array.cards[0].cardRank)
 
 }
