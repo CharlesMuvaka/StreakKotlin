bff2b75e31b6624b53caf5547ecccd0433634c4a
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-24 12:07:06 +0300

    create a method to display the player cards.

--- src/main/kotlin/PlayerHand.kt
+++ src/main/kotlin/PlayerHand.kt
@@ -1,14 +1,25 @@
 class PlayerHand(private val name:String, private val capacity:Int) {
     var playingCards: DynamicArray = DynamicArray()
-    private val playerName = name
+    val playerName = name
     val cardsToPlayWith = capacity
 }
 
 fun PlayerHand.assignPlayingCards(){
     val deck: CardDeck = CardDeck()
     for (i in 0 until this.cardsToPlayWith){
+        deck.allCards.shuffleCards()
         val card:Card = deck.allCards.cards[i]
         this.playingCards.addCard(card)
         deck.allCards.removeCard(card)
     }
+}
+
+fun PlayerHand.displayCards(): String{
+    val newLine = "\n"
+    var playerCards: String = "${this.playerName} has: $newLine"
+
+    for (card in this.playingCards.cards){
+        playerCards += "Card: ${card.cardRank} of ${card.cardSuit._name} ${card.cardSuit._color} $newLine"
+    }
+    return playerCards
 }
