5b4af90f32878b2e6dd07cca8a71e8b8e889137c
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-24 10:12:58 +0300

    create a method to shuffle the cards in the dynamic array.

--- src/main/kotlin/DynamicArray.kt
+++ src/main/kotlin/DynamicArray.kt
@@ -1,3 +1,5 @@
+import kotlin.random.Random
+
 class DynamicArray() {
     var cards: ArrayList<Card> = arrayListOf()
 
@@ -12,5 +14,15 @@ fun DynamicArray.removeCard(cardToBeRemoved: Card){
             cards.removeAt(cards.indexOf(card))
         }
         break
+    }
+}
+
+fun DynamicArray.shuffleCards(){
+    for (i in 0 until this.cards.size){
+        val randomNumber: Int = Random.nextInt(this.cards.size) //generates a random number
+
+        val newCard:Card = this.cards[i] //getting the card at the current iteration
+        this.cards[i] = this.cards[randomNumber] //assigning the card at the current iteration to a card from the rando number
+        this.cards[randomNumber] = newCard
     }
 }
