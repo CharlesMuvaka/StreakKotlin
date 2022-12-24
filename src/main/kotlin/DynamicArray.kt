892f941596602a050b27a67ecfb983abc90c8aa1
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-24 09:44:21 +0300

    create an extensive function to remove a card to the dynamic array

--- src/main/kotlin/DynamicArray.kt
+++ src/main/kotlin/DynamicArray.kt
@@ -6,3 +6,11 @@ class DynamicArray() {
 fun DynamicArray.addCard(card: Card){
     cards.add(card)
 }
+fun DynamicArray.removeCard(cardToBeRemoved: Card){
+    for (card in this.cards){
+        if (card.checkCardMatching(cardToBeRemoved)){
+            cards.removeAt(cards.indexOf(card))
+        }
+        break
+    }
+}
