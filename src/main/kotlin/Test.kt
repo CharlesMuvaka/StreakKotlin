4165a340bb7baba3b014bc029f5e1fc2e9ff1beb
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-04 23:17:37 +0300

    test whether the players cards can be sorted in ascending order.

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,31 +1,39 @@
 fun main() {
     val card = Card("2", 2,CardSuit("hearts", "red"))
     val cardOne = Card("3", 3,CardSuit("spades", "black"))
-    val cardTwo = Card("4", 3,CardSuit("clubs", "red"))
-    println(card.cardRank)
-    println(card.checkCardMatching(cardOne))
+    val cardTwo = Card("J", 11,CardSuit("clubs", "red"))
+//    println(card.cardRank)
+//    println(card.checkCardMatching(cardOne))
+    val  customArrayList = CustomArrayList(5);
+
+
+//    println(customArrayList.array.size)
+//    println(customArrayList.array[1])
+//    println(customArrayList.array[2])
 
     val array:DynamicArray = DynamicArray()
 
     array.addCard(card)
     array.addCard(cardOne)
     array.addCard(cardTwo)
-    println(array.cards.size)
-    array.removeCard(card)
-    println(array.cards.size)
-    println(array.cards[0].cardRank)
+//    println(array.cards.size)
+//    println(array.getCardByValue(2).getCardStringValue())
+//    println(array.cards[0].cardRank)
 
     array.shuffleCards()
 
-    println(array.cards[0].cardRank)
+//    println(array.cards[0].cardRank)
 //    println(getDefaultCardValues("J"))
     val deck: CardDeck = CardDeck()
-    println(deck.allCards.cards.size)
+//    println(deck.allCards.cards.size)
 
     val player = PlayerHand("Charles", 5);
     player.assignPlayingCards()
 //    player.playingCards.shuffleCards()
     println(player.displayCards())
+    println(player.sortCards())
+    println(player.displayCards())
+
 
 
 }
