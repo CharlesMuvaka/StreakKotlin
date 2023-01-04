662fb95a59b57105a5908b25fc477a698bcc6711
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-04 23:15:44 +0300

    rename member variables of the class

--- src/main/kotlin/CardSuit.kt
+++ src/main/kotlin/CardSuit.kt
@@ -1,5 +1,5 @@
 //create a blue print to initialize a single card suit instance
 class CardSuit(name: String, color: String) {
-    val _name: String = name
-    val _color: String = color
+    var _name: String = name
+    var _color: String = color
 }
