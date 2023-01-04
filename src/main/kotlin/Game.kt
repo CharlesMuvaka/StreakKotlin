db01cfd44f5da1e316ca68db9d3df214308b2fd0
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-05 00:25:53 +0300

    create a method to initialise a single player game

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,16 +1,33 @@
 import java.util.Scanner
 
 class Game {
+    private val reader = Scanner(System.`in`)
     init {
        startGame()
     }
 
     private fun startGame(){
-        val reader = Scanner(System.`in`)
         val start = "------------------------------Welcome to Streak----------------------------\n" +
                 "Click 1 for single game or 2 for two players"
         println(start)
         val gameToBePlayed = reader.nextInt()
-        println(gameToBePlayed)
+
+        if (gameToBePlayed == 1){
+            singlePlayerGame()
+        }
+    }
+
+    private fun singlePlayerGame(){
+        val name = "Enter yor name"
+        val cards = "Enter number of playing cards"
+        println(name)
+        val playerName = reader.next()
+        println(cards)
+        val cardsToPlayWith = reader.nextInt()
+        val player = PlayerHand(playerName, cardsToPlayWith)
+        player.assignPlayingCards()
+        player.sortCards()
+        println(player.displayCards())
+        println(player.getScore())
     }
 }
